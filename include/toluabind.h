bc20aa2c4ada081fe7c8d57f1c5539ead75e4adc
zhangfengzhen <zfengzhen@gmail.com>
2014-04-26 07:46:43 +0800

    first commit

---
+++ include/toluabind.h
@@ -0,0 +1,8 @@
+/*
+** Lua binding: tolua
+** Generated automatically by tolua++-1.0.92 on Fri Apr 25 23:12:03 2014.
+*/
+
+/* Exported function */
+TOLUA_API int  tolua_tolua_open (lua_State* tolua_S);
+
